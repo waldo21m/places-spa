82e43f75a1ea2610dead4f8bcdd2a1ce0abb63b6
Eduardo Márquez <eduardo20.3263@gmail.com>
2020-06-03 00:52:19 -0400

    Updates

---
+++ precache-manifest.f05e6a4e337c27621175b36d79956988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48850767be5d28ccc50f",
+    "url": "/places-spa/static/css/main.ebde58c2.chunk.css"
+  },
+  {
+    "revision": "48850767be5d28ccc50f",
+    "url": "/places-spa/static/js/main.bc12633e.chunk.js"
+  },
+  {
+    "revision": "c04567e052b44e64cf07",
+    "url": "/places-spa/static/js/runtime~main.15ccc6aa.js"
+  },
+  {
+    "revision": "bc57f2ff3433cd661333",
+    "url": "/places-spa/static/js/2.7662f00c.chunk.js"
+  },
+  {
+    "revision": "1cb910729a6206d89d995fc3ca9a4418",
+    "url": "/places-spa/index.html"
+  }
+];
